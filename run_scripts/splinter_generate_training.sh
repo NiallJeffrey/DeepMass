35593ef1829bca974ff35c35dbad5501874539bd
Niall <niall.jeffrey.15@ucl.ac.uk>
2019-05-20 16:31:58 +0200

    fixed bash bug

--- run_scripts/splinter_generate_training.sh
+++ run_scripts/splinter_generate_training.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #PBS -S /bin/bash
 #PBS -q compute
-#PBS -N array_test
+#PBS -N training_data
 #PBS -l nodes=1:ppn=1
 #PBS -l walltime=100:00:00
 
@@ -13,6 +13,5 @@ cd /share/splinter/ucapnje/DeepMass/run_scripts
 ## this jobfile should be called as a job array: e.g. qsub -t 0-58
 PARTNAME=`printf "%02d" ${PBS_ARRAYID}`
 
-cd /home/ucapnje/job_array_example
 
 python generate_training.py $PARTNAME
