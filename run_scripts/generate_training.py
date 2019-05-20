fee603f4cf3881dfcc35d2091a3da7abac6e24a8
Niall <niall.jeffrey.15@ucl.ac.uk>
2019-05-20 17:07:42 +0200

    complete splinter script

--- run_scripts/generate_training.py
+++ run_scripts/generate_training.py
@@ -2,23 +2,36 @@
 import numpy as np
 import sys
 import os
+import time
 
 from deepmass import map_functions as mf
-from deepmass import lens_data as ld
-from deepmass import wiener
 
-
-command_line_index = sys.argv[1]
+command_line_index = int(sys.argv[1])
 
 # kappa healpix directory
 kappa_dir = '/share/splinter/ucapnje/picola_for_peaks/l-picola_peak_statistics/kappaA/kappa_nicaea_rescaled/'
 
 list_kappa = os.listdir(kappa_dir)
 
+print(list_kappa[command_line_index])
 
-print(list_kappa[int(command_line_index)])
-
-
-picola_file = kappa_dir + str(list_kappa[int(command_line_index)])
+picola_file = kappa_dir + str(list_kappa[command_line_index])
 
 print(picola_file)
+
+# Load power spectrum and covariance matrix
+power_spectrum = np.load('../picola_training/temp_wiener_power_fiducial.py')
+Ncov = np.load('../picola_training/Ncov.npy')
+
+# make training data
+output_file_base = '../picola_training/nicaea_rescaled/training_data' + str(command_line_index) + '/sv_training_'
+
+
+t = time.time()
+mf.generate_sv_maps(picola_file,
+                    '../mice_mock/cat_DES_SV_zmean_final.fits',
+                    output_file_base,
+                    n_outputs = 40, power = power_spectrum, Ncov=Ncov,  sigma_eps=0.286)
+
+print(time.time() -t)
+
--- run_scripts/splinter_generate_training.sh
+++ run_scripts/splinter_generate_training.sh
@@ -3,6 +3,7 @@
 #PBS -q compute
 #PBS -N training_data
 #PBS -o /share/splinter/ucapnje/DeepMass/run_scripts/bash_outputs/training_data
+#PBS -e /share/splinter/ucapnje/DeepMass/run_scripts/bash_outputs/training_data
 #PBS -l nodes=1:ppn=1
 #PBS -l walltime=100:00:00
 
@@ -14,5 +15,6 @@ cd /share/splinter/ucapnje/DeepMass/run_scripts
 ## this jobfile should be called as a job array: e.g. qsub -t 0-58
 PARTNAME=`printf "%02d" ${PBS_ARRAYID}`
 
+mkdir ../picola_training/nicaea_rescaled/training_data$PARTNAME
 
 python generate_training.py $PARTNAME
