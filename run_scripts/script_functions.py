044858cd9f6480cae3c1160d288bcd6010db6830
Niall <niall.jeffrey.15@ucl.ac.uk>
2019-05-27 14:54:39 +0200

    rearranged generator script without wiener

--- run_scripts/script_functions.py
+++ run_scripts/script_functions.py
@@ -77,3 +77,32 @@ def plot_cnn(clean, noisy, reconstructed, output_file, vmin=0.3, vmax=0.7):
 
     plt.savefig(output_file), plt.close()
 
+
+def plot_noisy_clean(clean, noisy, output_file, vmin=0.3, vmax=0.7):
+    """
+    plots the clean maps, noisy maps and reconstruction
+    :param clean:
+    :param noisy:
+    :param reconstructed:
+    :param vmin: minimum colour on imshow
+    :param vmax: maximum colour on imshow
+    :param output_file: saves to a file
+    """
+
+    n = 6  # how many images displayed
+    n_images = len(clean[:, 0, 0, 0])
+    random_image_index = random.randint(0, n_images - 6)
+    plt.figure(figsize=(20, 11))
+    for i in range(n):
+        # display original
+        plt.subplot(2, n , i + 1)
+        plt.imshow(clean[i+ random_image_index, :, :, 0],
+                   origin='lower', cmap='inferno', clim=(vmin,vmax))
+        plt.axis('off'), plt.colorbar(fraction=0.046, pad=0.04)
+
+        plt.subplot(2, n, i + 1 + n)
+        plt.imshow(noisy[i+ random_image_index, :, :, 0],
+                   origin='lower', cmap='inferno', clim=(vmin,vmax))
+        plt.axis('off'), plt.colorbar(fraction=0.046, pad=0.04)
+
+    plt.savefig(output_file), plt.close()
