ee49e314cefae86d9a950241bd0e16917f202bf9
david-w-a <60277474+david-w-a@users.noreply.github.com>
2021-09-20 16:09:06 +0100

    Reorganized.

--- setup.py
+++ setup.py
@@ -4,13 +4,13 @@ from setuptools import setup, find_packages
 import sys
 
 setup(name='deepmass',
-      version='0.1',
+      version='1.0',
       description='Deep learning mass maps',
       author='Niall Jeffrey',
       url='https://github.com/NiallJeffrey/DeepMass',
       packages=find_packages(),
       install_requires=[
-          "keras>=v2.2.4",
+          "tensorflow>=2.3.0",
           "numpy"
       ])
 
